8399a5989d59050a71b47e700ec36eb76ea79e4b
Mei Huang <meihuang8328@gmail.com>
2024-04-28 20:52:18 -0700

    scooter class completed

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,30 @@
 class Scooter {
-  // scooter code here
+
+  static nextSerial = 1;
+  
+  constructor(station){
+      this.station = station;
+      this.user = null;
+      this.charge = 100;
+      this.isBroken = false;
+      this.serial = Scooter.nextSerial++
+  }
+
+  rent(user) {
+      if (this.charge > 20 && !this.isBroken) {
+          this.user = user;
+          this.station = null;
+      } else {
+          throw new Error("Scooter needs to charge or scooter needs repair.");
+      }
+  }
+
+  dock(station) {
+      this.station = station;
+      this.user = null;
+  }
+
+  
 }
 
-module.exports = Scooter
+module.exports = Scooter;
